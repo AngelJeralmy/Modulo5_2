4e8cabb83c6efc7f9fc832225cfef8c4ba8fd717
Ángel Zambrano <015angelzambrano@gmail.com>
2024-01-13 15:39:18 -0300

    Desafío2_5

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:"https://AngelJeralmy.github.io/Modulo5_2"
+})
